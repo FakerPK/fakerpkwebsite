02787c6f86c0c2c4f32294c47a2194e4838e8f07
FakerPK <faiqse7j@gmail.com>
2025-09-13 20:39:16 +0500

    Initial commit

--- src/components/Contact/socialData.ts
+++
@@ -1,8 +0,0 @@
-import { Github, Twitter, Linkedin, Mail } from 'lucide-react';
-
-export const socialLinks = [
-  { icon: Github, href: "#" },
-  { icon: Twitter, href: "#" },
-  { icon: Linkedin, href: "#" },
-  { icon: Mail, href: "#" },
-];
--- src/components/Navbar/navData.ts
+++
@@ -1,8 +0,0 @@
-import { Home, Briefcase, User, Mail } from 'lucide-react';
-
-export const navItems = [
-  { icon: Home, label: 'Home' },
-  { icon: Briefcase, label: 'Work' },
-  { icon: User, label: 'About' },
-  { icon: Mail, label: 'Contact' },
-];
